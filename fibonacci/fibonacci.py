700aac5722a81e5fed97d4c08823de49ad143e12
Tássyla Lima <56099401+tatitassyla@users.noreply.github.com>
2021-11-04 15:27:40 -0300

    fibonacci

---
+++ fibonacci/fibonacci.py
@@ -0,0 +1,20 @@
+tb = [-1 for _ in range(1005)]
+
+def fib_dp (n):
+    global tb
+    
+    if n == 0: return 0
+    if n == 1: return 1
+    if tb[n] != -1: return tb[n]
+
+    tb[n] = fib_dp(n-1) + fib_dp(n-2)
+    return tb[n]
+
+while True:
+    try: 
+
+        n = int(input())
+        print(f'f({n}) = {fib_dp(n)}')
+
+    except:
+        break
